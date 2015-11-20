53ffd8eb99b2f6ab26fb4e490c48e8aedc7cb6bd
SilverAmadeus <Ivan@Ivans-MacBook-Pro.local>
2015-11-20 03:30:30 -0600

    Print multiple
    Imprime en un archivo todas las lineas hasta que se le indique
    escribiendo “quit”

--- Archive_sys.py
+++ Archive_sys.py
@@ -40,6 +40,29 @@ def ar_pos(name): #Posicion actual del puntero
     except:
         print('Something went wrong! Cant tell what?')
 
+def ar_w(name):
+    try:
+        if os.path.isfile(name):
+            arch = open(name, "r")
+            print "\t\tEscribe 'quit para salir del editor\n\t\tESTE METODO SOBRE ESCRIBE EL ARCHIVO\n" \
+                  "\t\tContenido del archivo:\n"
+            for line in arch.readlines():
+                print line
+            arch.close()
+            arch = open(name, "w+")
+            lines = []
+            lp = 0
+            while(lp != 'quit\n'):
+                lp = raw_input(": ")+"\n"
+                if lp != 'quit\n':
+                    lines.append(lp)
+            arch.writelines(lines)
+            arch.close()
+
+        else:
+            print "El fichero no existe"
+    except:
+        print('Something went wrong! Cant tell what?')
 
 #FORDIRECTORIES
 def deleteFile(name):
@@ -54,19 +77,25 @@ while (r != 'q'):
 
     print("\t-- Comandos: To be added, h: Para ayuda --")
     r = raw_input(": ")
-    print r
     if(r == 'h'):
         print("\t\tdir_map: Muestra contenido del directorio\n"
               + "\t\tar_c: Crea un archivo\n"
+              + "\t\tar_r: Muestra contenido de un archivo\n"
+              + "\t\tar_w: Escribe en un archivo (Sobre escribe contenido\n"
               + "\t\tq: Salir del sistema\n")
         raw_input("Press Enter to continue...")
     elif(r == 'ar_c'):
         # Llamada a funcion con raw_input como valor que se espera en el teclado, espero no te nortee. :P
         ar_c(raw_input("Nombre del archivo: ")+'.txt')
+        raw_input("Press Enter to continue...")
     elif(r == 'ar_r'):
         ar_r(raw_input("Nombre del archivo: ")+'.txt')
+        raw_input("Press Enter to continue...")
     elif(r == 'dir_map'):
         dir_map()
+        raw_input("Press Enter to continue...")
+    elif(r == 'ar_w'):
+        ar_w(raw_input("Nombre del archivo: ")+'.txt')
     else:
         print("Comando invalido\n")
 print("Exiting...")
