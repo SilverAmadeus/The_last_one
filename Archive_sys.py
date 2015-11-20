14e030e4f7f95a76906f7937ecb094d974065d07
SilverAmadeus <Ivan@Ivans-MacBook-Pro.local>
2015-11-20 01:24:12 -0600

    Mapa del directorio
    function con mapa de directorio, igual la agrego al menu de opciones

--- Archive_sys.py
+++ Archive_sys.py
@@ -21,8 +21,10 @@ def ar_c(name):
 def deleteFile(name):
 	os.remove(name) #name is a string
 
-
 	
+def dir_map():
+	subprocess.call(['tree'])	
+
 r = 0
 print("\n\n\t\t.:: Sistema de Archivos ::.\n")
 while (r != 'q'):
@@ -37,6 +39,9 @@ while (r != 'q'):
 	if(r == 'ar_c'):
 		# Llamada a funcion con raw_input como valor que se espera en el teclado, espero no te nortee. :P
 		ar_c(raw_input("Nombre del archivo: ")+'.txt') 
+
+	if r == 'dir_map':
+		dir_map()
 	elif(r != 'q'):
 		print("Comando invalido\n")
 print("Exiting...")
